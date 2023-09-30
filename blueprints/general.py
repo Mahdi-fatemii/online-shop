e7c4392df2af8ed78b1bc7180566b1a7b04d3297
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-09-30 15:31:25 +0330

    route (aboute) added

--- blueprints/general.py
+++ blueprints/general.py
@@ -4,5 +4,12 @@ app = Blueprint("general", __name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def main():  # put application's code here
     return 'this is main page'
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return 'about us'
+
+
