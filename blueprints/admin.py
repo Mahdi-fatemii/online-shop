3bc53c71c4e5242a2b16a2eedc313afc55077b89
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-10-02 22:04:33 +0330

    base html,admin login,admin dashboard,products

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, render_template, abort, request, redirect,session
+from flask import Blueprint, render_template, abort, request, redirect, session
 import config
+from models.product import Product
+from extentions import db
 
 app = Blueprint("admin", __name__)
+
+
+@app.before_request
+def before_request():
+    if session.get('admin_login', None) == None and request.endpoint != "admin.login":
+        abort(403)
+
 
 
 @app.route('/admin/login', methods=["POST", "GET"])
@@ -18,10 +27,30 @@ def login():  # put application's code here
     else:
         return render_template("/admin/login.html")
 
+
 @app.route('/admin/dashboard', methods=["GET"])
 def dashboard():  # put application's code here
-    if session.get('admin_login', None) == None:
-        abort(403)
+    return render_template("/admin/dashboard.html")
 
-    return "dashboard"
 
+@app.route('/admin/dashboard/products', methods=["GET", "POST"])
+def products():
+    if request.method == "GET":
+        products = Product.query.all()
+        return render_template("/admin/products.html", products=products)
+    else:
+        name = request.form.get('name', None)
+        description = request.form.get('description', None)
+        price = request.form.get('price', None)
+        status = request.form.get('status', None)
+
+        p = Product(name=name, description=description, price=price)
+        if status == None:
+            p.status = 0
+        else:
+            p.status = 1
+
+        db.session.add(p)
+        db.session.commit()
+
+        return "done"
--- models/product.py
+++ models/product.py
@@ -5,6 +5,7 @@ from extentions import db
 class Product(db.Model):
     __tablename__ = "products"
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False, index=True)
+    name = Column(String, nullable=False, index=True)
     description = Column(String, nullable=False, index=True)
     price = Column(Integer, nullable=False, index=True)
+    status = Column(Integer, nullable=False, index=True)
